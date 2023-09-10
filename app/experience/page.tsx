f02f22e3db423fbff5da9ebdec67f2786e1adb8a
evanmarshall-dev <me@evanmarshall.dev>
2023-09-10 20:00:44 -0300

    PORTFOLIO: Starting to build the work experience section. Added smooth scroll to the page.

---
+++ app/experience/page.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+
+const experience = (props: Props) => {
+  return (
+    <div className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-left md:flex-row justify-evenly">
+      <h3 className="absolute top-24 uppercase tracking-[10px] text-gray-500 text-2xl">
+        My Work Experience
+      </h3>
+    </div>
+  );
+};
+
+export default experience;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Hero from "./hero/page";
 import About from "./about/page";
+import WorkExperience from "./experience/page";
 
 export default function Home() {
   return (
-    <div className="z-0 h-screen overflow-scroll snap-y snap-mandatory">
+    <div className="z-0 h-screen overflow-scroll snap-y snap-mandatory scroll-smooth">
       <section id="hero" className="snap-start">
         <Hero />
       </section>
       <section id="about" className="snap-center">
         <About />
+      </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience />
       </section>
     </div>
   );
