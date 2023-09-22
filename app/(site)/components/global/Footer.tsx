aecf6e5a8f17b37e9384de4daec9f5a545e7f26e
evanmarshall-dev <me@evanmarshall.dev>
2023-09-22 19:36:16 -0300

    🔧 chore(vscode): update cSpell words 🔨 refactor(footer): remove commented out code

--- app/(site)/components/global/Footer.tsx
+++ app/(site)/components/global/Footer.tsx
@@ -6,13 +6,6 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      {/* <Link href="#hero">
-        <div className="py-4 sticky w-full cursor-pointer bottom-5">
-          <div className="flex items-center justify-center">
-            <FaJedi className="w-10 h-10 text-[rgb(245,245,245)] hover:text-[rgb(255,83,61)]" />
-          </div>
-        </div>
-      </Link> */}
       <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400 border-t-2 border-[#ab987a]/70">
         <small className=" duration-200 font-mono">
           <Link href="/">
