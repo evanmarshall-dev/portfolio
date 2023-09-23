85d12932b8ffc7deed6db65a89483ae2dde19abd
evanmarshall-dev <me@evanmarshall.dev>
2023-09-23 01:23:38 -0300

    📦 chore(database): update data and logs binary files 📝 docs(migrations): add migration script for updating test collection name

---
+++ pb_migrations/1695441751_updated_test.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("nmj8gexv71nepxt")
+
+  collection.name = "notes"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("nmj8gexv71nepxt")
+
+  collection.name = "test"
+
+  return dao.saveCollection(collection)
+})
