cd5258c134bab238aad718652313588e05e5eea5
evanmarshall-dev <me@evanmarshall.dev>
2023-09-26 09:19:31 -0300

    🔥 chore(prettier): remove .prettierrc and add prettier.config.js 🔧 chore(prettier): update prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
+
+module.exports = {
+  config,
+  tailwindFunctions: ["tw"],
+  tailwindConfig: "./styles/tailwind.config.js",
+};
