6c3f198f0a6438792ef148db7e33ee44a374c1a0
evanmarshall-dev <me@evanmarshall.dev>
2023-09-26 09:10:46 -0300

    🔧 chore(eslint): update eslint configuration 🔧 chore(husky): add lint-staged command to pre-commit hook 🔧 chore(prettier): update prettier configuration 🔧 chore(prettierignore): update prettierignore file 🔧 chore(prettierrc): add prettierrc configuration 🐛 fix(components): remove unused imports in Hero component 🔧 chore(package): add type-check script to package.json

--- app/(site)/components/(homepage)/Hero.tsx
+++ app/(site)/components/(homepage)/Hero.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+
 import BackgroundCircles from "../BackgroundCircles";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
-  const [text, count] = useTypewriter({
+  const [text] = useTypewriter({
     words: [
       "Hi friends! it's me, Evan Marshall",
       "Welcome to my portfolio website!",
@@ -19,37 +20,37 @@ export default function Hero() {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen overflow-hidden text-center space-y-8">
+    <div className='flex flex-col items-center justify-center h-screen overflow-hidden text-center space-y-8'>
       <BackgroundCircles />
       <Image
         width={128}
         height={128}
-        className="relative object-cover mx-auto rounded-full"
-        src="/evan-marshall-profile.png"
-        alt="AI generated portrait of Evan Marshall"
+        className='relative object-cover mx-auto rounded-full'
+        src='/evan-marshall-profile.png'
+        alt='AI generated portrait of Evan Marshall'
         priority
       />
-      <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>
           Web Developer
         </h2>
-        <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
-          <span className="mr-3">{text}</span>
-          <Cursor cursorColor="rgb(255,83,61)" />
+        <h1 className='px-10 text-5xl font-semibold lg:text-6xl'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='rgb(255,83,61)' />
         </h1>
-        <div className="pt-5">
-          <Link href="#about">
-            <button className="heroButton">About</button>
+        <div className='pt-5'>
+          <Link href='#about'>
+            <button className='heroButton'>About</button>
           </Link>
-          <Link href="#experience">
-            <button className="heroButton">Experience</button>
+          <Link href='#experience'>
+            <button className='heroButton'>Experience</button>
           </Link>
-          <Link href="#skills">
-            <button className="heroButton">Skills</button>
+          <Link href='#skills'>
+            <button className='heroButton'>Skills</button>
           </Link>
-          <Link href="#projects">
+          <Link href='#projects'>
             {" "}
-            <button className="heroButton">Projects</button>
+            <button className='heroButton'>Projects</button>
           </Link>
         </div>
       </div>
