9f5a6a42fa4b8a58c8199cfd1cf9d946a4539406
evanmarshall-dev <me@evanmarshall.dev>
2024-11-19 16:04:22 -0400

    PORTFOLIO: Updated footer component to have a dynamically generated copyright year. Also minor updates to alignment for mobile.

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 function Footer() {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    // Update the year when the component mounts
+    setYear(new Date().getFullYear());
+  }, []);
+
   return (
     <div className="py-6 mt-12 lg:mt-18 sm:pb-36 sm:py-12">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
         <div className="pb-8 mb-2 border-t-0 md:border-t-2 border-gray-300 dark:border-white-300">
           <div className="flex flex-col items-center justify-between lg:flex-row text-center md:text-left">
             <p>
-              Copyright &copy; 2022 - evanmarshall
+              &copy; {`${year} - evanmarshall`}
               <span className="text-sunset_orange">.</span>
               <span className="text-sandrift">dev</span>, All rights reserved
               <span className="text-sunset_orange">.</span>
             </p>
-            <div className="flex flex-wrap pt-2 space-x-2 font-medium sm:space-x-4 lg:pt-0">
+            <div className="flex flex-col md:flex-row flex-wrap pt-2 space-x-2 font-medium sm:space-x-4 lg:pt-0">
               <a
                 href="https://www.linkedin.com/in/evan-marshall-dev/"
                 className="transition-colors hover:text-sandrift"
