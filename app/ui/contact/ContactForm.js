8e3c25a31ccfa1bbc15587fa2eb83debe78c067b
evanmarshall-dev <me@evanmarshall.dev>
2024-11-20 00:07:18 -0400

    PORTFOLIO: Updated ContactForm.js environment variables to remove NEXT_PUBLIC to make production ready. Left development environment versions there but commented out.

--- app/ui/contact/ContactForm.js
+++ app/ui/contact/ContactForm.js
@@ -11,8 +11,11 @@ export default function ContactForm() {
     script.addEventListener("load", () => {
       if (window.hbspt) {
         window.hbspt.forms.create({
-          portalId: process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID,
-          formId: process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID,
+          // TODO: Uncomment NEXT_PUBLIC versions for dev environment.
+          // portalId: process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID,
+          // formId: process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID,
+          portalId: process.env.HUBSPOT_PORTAL_ID,
+          formId: process.env.HUBSPOT_FORM_ID,
           target: "#hubspotForm",
         });
       }
