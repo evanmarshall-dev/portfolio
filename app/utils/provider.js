d0c67498ee04fb4119a0999b89b7460ea14b2dbb
evanmarshall-dev <me@evanmarshall.dev>
2025-01-31 13:14:50 -0400

    PORTFOLIO: Updated ENV VAR for posthog to prepend NEXT_PUBLIC_. TODO: Make sure this is ok and does not expose key.

--- app/utils/provider.js
+++ app/utils/provider.js
@@ -4,9 +4,9 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.POSTHOG_KEY, {
-    api_host: process.env.POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always",
   });
 }
 export function CSPostHogProvider({ children }) {
