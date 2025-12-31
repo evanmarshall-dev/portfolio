e26244a22e1ccedcace27e9f53041a676d3cfda2
evanmarshall-dev <me@evanmarshall.dev>
2025-12-31 02:11:08 -0400

    feat: add Courtesy Flushers project to portfolio

--- app/ui/Work.js
+++ app/ui/Work.js
@@ -1,84 +1,81 @@
-import Image from "next/image";
-import Link from "next/link";
-import glooscapPic from "@/public/images/glooscap_thumb.png";
-import hearmstrongPic from "@/public/images/hearmstrong_thumb.png";
-import hockleyDentistryPic from "@/public/images/hockley-dentistry_thumb.png";
-import gatesInsurancePic from "@/public/images/gatesinsurance_thumb.png";
-import TripleETechPic from "@/public/images/tripleetech_thumb.png";
-import safeguardPic from "@/public/images/safeguard_thumb.png";
-import steppingStonePic from "@/public/images/stepping-stone_thumb.png";
-import eliteAutoPic from "@/public/images/elite-auto_thumb.png";
-import acadiaRefrigePic from "@/public/images/acadia-refrige_thumb.png";
-import fleetworxPic from "@/public/images/fleetworx_thumb.png";
+import Image from 'next/image';
+import Link from 'next/link';
+import glooscapPic from '@/public/images/glooscap_thumb.png';
+import hearmstrongPic from '@/public/images/hearmstrong_thumb.png';
+import hockleyDentistryPic from '@/public/images/hockley-dentistry_thumb.png';
+import gatesInsurancePic from '@/public/images/gatesinsurance_thumb.png';
+import TripleETechPic from '@/public/images/tripleetech_thumb.png';
+import safeguardPic from '@/public/images/safeguard_thumb.png';
+import steppingStonePic from '@/public/images/stepping-stone_thumb.png';
+import eliteAutoPic from '@/public/images/elite-auto_thumb.png';
+import acadiaRefrigePic from '@/public/images/acadia-refrige_thumb.png';
+import fleetworxPic from '@/public/images/fleetworx_thumb.png';
+import courtesyFlushersPic from '@/public/images/courtesy-flushers_thumb.png';
 
 const projects = [
   {
-    href: "https://glooscapfirstnation.com/",
+    href: 'https://glooscapfirstnation.com/',
     src: glooscapPic,
   },
   {
-    href: "https://www.valleyburnerservice.com/",
+    href: 'https://www.valleyburnerservice.com/',
     src: hearmstrongPic,
   },
-  // {
-  //   href: "https://www.quarterdeck.ca/",
-  //   src: quarterdeckPic,
-  // },
   {
-    href: "https://hockleyfamilydentistry.ca/",
+    href: 'https://www.courtesyflushers.ca/',
+    src: courtesyFlushersPic,
+  },
+  {
+    href: 'https://hockleyfamilydentistry.ca/',
     src: hockleyDentistryPic,
   },
   {
-    href: "https://gatesinsurance.ca/",
+    href: 'https://gatesinsurance.ca/',
     src: gatesInsurancePic,
   },
   {
-    href: "https://tripleetech.ca/",
+    href: 'https://tripleetech.ca/',
     src: TripleETechPic,
   },
-  // {
-  //   href: "https://kentfields.ca/",
-  //   src: kentfieldsPic,
-  // },
   {
-    href: "https://safeguardservices.ca/",
+    href: 'https://safeguardservices.ca/',
     src: safeguardPic,
   },
   {
-    href: "https://steppingstonepreschool.ca/",
+    href: 'https://steppingstonepreschool.ca/',
     src: steppingStonePic,
   },
   {
-    href: "https://www.eliteautoandglass.ca/",
+    href: 'https://www.eliteautoandglass.ca/',
     src: eliteAutoPic,
   },
   {
-    href: "https://acadiarefrigeration.com/",
+    href: 'https://acadiarefrigeration.com/',
     src: acadiaRefrigePic,
   },
   {
-    href: "https://fleetworxinc.ca/",
+    href: 'https://fleetworxinc.ca/',
     src: fleetworxPic,
   },
 ];
 
 function Work() {
   return (
-    <div className="container flex items-center justify-between w-full px-8 mx-auto mt-64 md:px-14 lg:px-24">
-      <section className="w-full text-center md:text-left">
-        <h2 id="work" className="secondary-title">
+    <div className='container flex items-center justify-between w-full px-8 mx-auto mt-64 md:px-14 lg:px-24'>
+      <section className='w-full text-center md:text-left'>
+        <h2 id='work' className='secondary-title'>
           My Web Developer Portfolio
         </h2>
-        <p className="section-paragraph">
+        <p className='section-paragraph'>
           Please enjoy a curated collection of my great clients
-          <span className="text-sunset_orange">.</span>
+          <span className='text-sunset_orange'>.</span>
         </p>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
           {projects.map((project, index) => {
             const domain = new URL(project.href).hostname;
 
             return (
-              <Link key={index} href={project.href} target="_blank">
+              <Link key={index} href={project.href} target='_blank'>
                 <Image
                   src={project.src}
                   alt={`Thumbnail of ${domain} website, created by Evan Marshall: Professional Web Developer.`}
